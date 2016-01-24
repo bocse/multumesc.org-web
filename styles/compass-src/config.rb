3dc10a0cfb58cfebee4c9a3797ec4b996a226f37
olesea <olesea.harea@committed.ro>
2016-01-24 18:43:08 +0200

    cleans css, adds styles folder for all styles

---
+++ styles/compass-src/config.rb
@@ -0,0 +1,14 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "../"
+sass_dir = "src"
+images_dir = "../../images"
+javascripts_dir = "../../js"
+line_comments = false
+relative_assets = true
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
