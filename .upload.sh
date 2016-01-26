8a93aaf9c13a38ce4aa9da2448e5c51e345d66fb
Bogdan Bocse <bogdan.bocse@gmail.com>
2016-01-26 22:57:53 +0200

    clean-up

--- .upload.sh
+++ .upload.sh
@@ -1,1 +1,1 @@
-aws s3 sync . s3://multumesc  --exclude '*.json'
+aws s3 sync . s3://multumesc  --exclude '*.json' --exclude '.git/*' --exclude 'README.md' --exclude '.upload.sh' --exclude '.idea/*' --exclude '.gitignore' --delete
